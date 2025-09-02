0ef8d92c3673598954450cd74f7c30f71d23d5a7
Pushkar Labh <pushkar.labh313@gmail.com>
2025-09-02 01:34:20 +0000

    Updated Login and signup Page V(7) ,
    Find this element:
    
      <LoginForm>
    
    with

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -5,7 +5,7 @@ import SignupMarketing from '@/components/sections/signup-marketing';
 
 export default function SignupPage() {
   return (
-    <div className="flex min-h-dvh flex-col bg-[#4169E1]/[0.07] md:bg-[#FAFAFA]">
+    <div className="flex min-h-dvh flex-col bg-[#FAFAFA]">
       <Header />
       <main className="flex-1 flex items-center justify-center pt-16">
         <div className="w-full max-w-6xl p-4 md:py-20">
